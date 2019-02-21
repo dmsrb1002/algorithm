6d70ab4a65545e30830efb7dcc9ee1eb057bc8f8
dmsrb1002 <39038213+dmsrb1002@users.noreply.github.com>
2019-02-21 12:37:20 +0900

    손익 분기점

---
+++ 1700/1712.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<algorithm>
+#include<math.h>
+using namespace std;
+
+
+int main()
+{
+	int a, b, c;
+	cin >> a >> b >> c;
+	if (c > b) {
+		cout << a / (c - b) + 1 << endl;
+	}
+	else {
+		cout << -1 << endl;
+	}
+}
